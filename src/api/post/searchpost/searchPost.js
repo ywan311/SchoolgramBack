1c5f710c3cb21f6b47e73c4b994984371ed95be6
유완 <ywan311@naver.com>
2019-04-23 12:48:20 +0900

    update seeRooms

--- src/api/post/searchpost/searchPost.js
+++ src/api/post/searchpost/searchPost.js
@@ -1,11 +1,15 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { COMMENT_FRAGMENT, FULL_POST_FRAGMENT } from "../../../fragments";
 
 export default {
   Query: {
-    seeFullPost: async (_, args) => {
-      const { id } = args;
-      return prisma.post({ id }).$fragment(FULL_POST_FRAGMENT);
-    }
+    searchPost: async (_, args) =>
+      prisma.posts({
+        where: {
+          OR: [
+            { location_starts_with: args.term },
+            { caption_starts_with: args.term }
+          ]
+        }
+      })
   }
 };
